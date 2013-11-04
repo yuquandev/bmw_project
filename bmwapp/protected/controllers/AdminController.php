b66564a86b24df03b61d7cce4981184d321ab60e
yuquandev <yuquandev@163.com>
2013-11-04 23:58:56 +0800

    2

---
+++ bmwapp/protected/controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: zhaoquan
+ * Date: 13-11-4
+ * Time: 下午11:19
+ * To change this template use File | Settings | File Templates.
+ */
+
+class AdminController extends Controller {
+
+
+    public function actionIndex(){
+        echo 123;
+    }
+}
