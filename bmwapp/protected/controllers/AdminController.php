dd55e3b1367d2e93ec9547c9ae19ddfe39bfd37f
yuquandev <yuquandev@163.com>
2013-11-05 09:53:08 -0800

    123

--- bmwapp/protected/controllers/AdminController.php
+++ bmwapp/protected/controllers/AdminController.php
@@ -36,8 +36,9 @@ class AdminController extends Controller {
         if (!self::$is_login){
             $this->redirect("/index.php/admin/login");
         }
-        print_r($this->admin_user->get_admin_user_list());
-        $this->render("/admin/index");
+        #print_r($this->admin_user->get_admin_user_list());
+        $data = array("key"=>array(1,2,3));
+        $this->render("/admin/index",$data);
     }
 
     public function actionLogin(){
@@ -53,11 +54,11 @@ class AdminController extends Controller {
             echo "参数错误";
         }else {
             $userinfo = $this->admin_user->get_admin_info_by_name($username);
-            print_r(md5($password.$userinfo['salt']));
+
             if ( $userinfo['password'] != md5($password.$userinfo['salt'])){
                 echo "密码错误";
             }else {
-                $lifeTime = 60;
+                $lifeTime = 60*60;
                 setcookie('bmw_ad_ses', md5($userinfo['id'].$this->login_key.$userinfo['username']), time() + $lifeTime, "/");
                 setcookie('bmw_ad_username', $userinfo['username'], time() + $lifeTime, "/");
                 setcookie('bmw_ad_uid', $userinfo['id'], time() + $lifeTime, "/");
@@ -67,4 +68,41 @@ class AdminController extends Controller {
 
         $this->render("/admin/login");
     }
+
+    public function actionLogout(){
+        if (!self::$is_login){
+            $this->redirect("/index.php/admin/login");
+        }
+        setcookie('bmw_ad_ses', '', time() - 3600, "/");
+        setcookie('bmw_ad_username', '', time() - 3600, "/");
+        setcookie('bmw_ad_uid', '', time() - 3600, "/");
+        $this->redirect("/index.php/admin/login");
+    }
+
+    //异步获取表的字段名
+    public function actionColumns(){
+        $act = isset($_POST['act']) ? trim($_POST['act']) : '';
+        if(empty($act)){echo "";}
+        else {
+            $act = array("id","username","nickname","telephone","ip");
+            echo json_encode($act);
+        }
+    }
+
+    //异步获取表数据
+    public function actionDatajson(){
+        $table_name = isset($_GET['act']) ? trim($_GET['act']) : "";
+        $page = isset($_POST['page']) ? intval($_POST['page']) : 0;
+        $rows = isset($_POST['rows']) ? intval($_POST['rows']) : 0;
+        if(empty($table_name)){echo "";}
+        else {
+            $data = $this->user->get_user_list($page,$rows);
+            #print_r($data);
+            $count = $this->user->get_user_total();
+            #print_r($count);
+            $data = array("total"=>$count[0]['total'],"rows"=>$data);
+            echo json_encode($data);
+        }
+    }
+
 }
