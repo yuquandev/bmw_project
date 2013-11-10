d89d5aaf2fce4d060645f254587a046eeab8b8b3
yuquandev <yuquandev@163.com>
2013-11-09 19:58:57 -0800

    layout admin

--- bmwapp/protected/views/layouts/admin.php
+++ bmwapp/protected/views/layouts/admin.php
@@ -19,10 +19,13 @@
 <div data-options="region:'north'" style="overflow: hidden">
     <div style="padding:5px;border:1px solid #ddd">
         <a href="javascript:void(0);" class="easyui-linkbutton" data-options="plain:true" style="" onclick="add_car_dialog();">新建专题</a>
+        <a href="javascript:void(0);" class="easyui-linkbutton" data-options="plain:true" style="" onclick=";">新建管理员</a>
         <a href="/index.php/admin/logout" class="easyui-linkbutton" data-options="plain:true" style="float:right;">退出</a>
+        <a style="float:right;padding-top: 8px;"><?php echo $this->user_info['username'];?></a>
+        <div style="clear: both;"></div>
     </div>
 </div>
-<div data-options="region:'south',split:true" style="height:50px;"></div>
+<div data-options="region:'south',split:true" style="height:0px;"></div>
 <div data-options="region:'west',split:true" title="" style="width:200px;">
     <div class="easyui-accordion" data-options="fit:true,border:false" >
         <!--<div title="专题管理" data-options="" style="padding:10px;">
@@ -32,20 +35,24 @@
         </div>-->
         <?php foreach ($this->car_list as $k=>$v){?>
             <div title="专题页 - <?php echo $v['name'];?>" data-options="" style="padding:10px;">
+                <li style="padding-bottom: 10px;">[<a href="javascript:void(0);" onclick="">专题名称修改</a>]</li>
                 <li style="padding-bottom: 10px;">[<a href="javascript:void(0);" onclick="add_nav_dialog(<?php echo $v['type_id'];?>);">新建导航</a>]</li>
                 <?php foreach ($this->nav_list as $kk=>$vv){
                     if ($vv['type_id'] == $v['type_id']){?>
-                        <li style="padding-bottom: 10px;"><a href="javascript:void(0);" onclick="get_nav_info(<?php echo $vv['id'];?>);"><?php echo $vv['name'];?></a></li>
+                        <li style="padding-bottom: 10px;">&nbsp;&nbsp;&nbsp;&nbsp;<a href="javascript:void(0);" onclick="get_nav_info(<?php echo $vv['id'];?>);"><?php echo $vv['name'];?></a></li>
                     <?php }
                 }?>
                 <li style="padding-top: 10px; padding-bottom: 10px;"><a href="javascript:void(0);" onclick="ajax_get_columns('image_list_<?php echo $v['type_id'];?>','<?php echo $v['name'];?>图片列表');">图片列表 - <?php echo $v['name'];?></a></li>
             </div>
         <?php } ?>
-        <div title="作品管理" data-options="" style="padding:10px;">
-            <li style="padding-bottom: 10px;"><a href="javascript:void(0);" onclick="ajax_get_columns('works_list','用户作品列表');">用户作品列表</a></li>
+        <div title="用户作品管理" data-options="" style="padding:10px;">
+            <?php foreach ($this->car_list as $k=>$v){?>
+                <li style="padding-bottom: 10px;"><a href="javascript:void(0);" onclick="ajax_get_columns('works_list_<?php echo $v['type_id'];?>','<?php echo $v['name'];?>作品列表');"><?php echo $v['name'];?>作品列表</a></li>
+            <?php } ?>
         </div>
         <div title="用户管理" data-options="" style="overflow:auto;padding:10px;">
             <li style="padding-bottom: 10px;"><a href="javascript:void(0);" onclick="ajax_get_columns('user_list','用户列表');">用户列表</a></li>
+            <li style="padding-bottom: 10px;"><a href="javascript:void(0);" onclick="ajax_get_columns('admin_list','管理员列表');">管理员列表</a></li>
         </div>
 
     </div>
@@ -77,7 +84,7 @@
     <label class="lbInfo">上传图片：</label><div id="divFileProgressContainer"></div><br />
     <div><a href="javascript:void(0)" id="add_btn" class="l-btn" group="" ><span class="l-btn-left"><span class="l-btn-text"><div id="spanButtonPlaceholder"></div></span></span></a></div>
     <div id="thumbnails" >
-            <img style="margin: 5px; vertical-align: middle; opacity: 1;width: 300px;" src="" />
+        <img style="margin: 5px; vertical-align: middle; opacity: 1;width: 200px;" src="" />
     </div>
     <input id="t_img_url"  type="hidden" class="easyui-validatebox"  required="true" runat="server" /><br />
 </div>
